b6219f478cf58c101db219532b25a850cf81ccb5
Aleksandra <panidoris1@gmail.com>
2026-01-10 16:09:35 +0100

    fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
     implementation("com.github.almasb:fxgl:17.3") {
         exclude(group = "org.openjfx")
-        exclude(group = "org.jetbrains.kotlin")
     }
     testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
